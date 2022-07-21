1706fbe56c08ba790b8865fbe0b811f31ee63e33
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2022-07-21 13:38:48 +0700

    upload avatar

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -22,11 +22,9 @@
     >
     <div class="text-2xl text-center">User Profile</div>
     <div>
-      <img
-        src="../assets/avatar.png"
-        class="w-32 h-32 rounded-full m-auto mt-4 cursor-pointer"
-        @click="$refs.fileInputAvatar.click()"
-      />
+      <div class="block text-center" style="padding: 10px">
+        <el-avatar shape="square" :size="100" :src="url"></el-avatar>
+      </div>
     </div>
     <div class="mt-4">
       <span class="text-lg font-bold">Email: </span>
@@ -36,28 +34,29 @@
       <span class="text-lg font-bold">Name: </span>
       <b-form-input v-model="name" placeholder="Enter your name"></b-form-input>
     </div>
+    <div>
+      <span class="text-lg font-bold">Avatar_url: </span>
+      <b-form-input
+        v-model="url"
+        placeholder="Enter your avatar_url"
+      ></b-form-input>
+    </div>
     <b-button class="mt-4 border-2 text-xl p-4 save-btn" @click="saveUserInfo"
       >Save</b-button
     >
-    <input
-      ref="fileInputAvatar"
-      type="file"
-      accept="image/png, image/gif, image/jpeg"
-      style="display: none"
-      @input="UploadAvatar"
-    />
   </div>
 </template>
 
 <script>
-import { getUserInfo } from "../api/user";
-import { logout } from '../api/authentication';
+import { getUserInfo, updateUser } from "../api/user";
+import { logout } from "../api/authentication";
 
 export default {
   data() {
     return {
       email: "",
       name: "",
+      url: "",
     };
   },
   methods: {
@@ -68,8 +67,22 @@ export default {
         console.log(files[0]);
       }
     },
-    saveUserInfo() {
-      console.log("save");
+    async saveUserInfo() {
+      try {
+        const id = localStorage.getItem("userId");
+        const res = await updateUser(id, { name: this.name, avatar: this.url });
+        if (res) {
+          this.$message({
+            message: "編集できました！",
+            type: "success",
+          });
+        }
+      } catch (error) {
+        this.$message({
+          message: "エーラが発生した！",
+          type: "error",
+        });
+      }
     },
     logOut() {
       logout()
@@ -80,11 +93,12 @@ export default {
     },
   },
   async created() {
-    const id = localStorage.getItem("userId")
-    const userInfo = await getUserInfo(id)
-    this.email = userInfo.email
-    this.name = userInfo.name
-  }
+    const id = localStorage.getItem("userId");
+    const userInfo = await getUserInfo(id);
+    this.email = userInfo.email;
+    this.name = userInfo.name;
+    if (userInfo.avatar) this.url = userInfo.avatar;
+  },
 };
 </script>
 <style scoped>
